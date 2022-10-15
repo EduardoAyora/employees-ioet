828848fc25383a598b37a54eefb9f076a7ca7dc4
EduardoAyora <eduardoaayora64@gmail.com>
2022-10-15 00:15:06 -0500

    Add function isMatchInTime with implementation of hours

--- src/schedule/get-employees-matches-at-work.js
+++ src/schedule/get-employees-matches-at-work.js
@@ -6,6 +6,29 @@ const getEmployeesMatchesAtWork = (employeesAndScheduleString) => {
   const employeesAndSchedule = employeesAndScheduleArrayOfStrings.map(
     (employeeAndScheduleString) => new Employee(employeeAndScheduleString)
   )
+  const employeesMatches = employeesAndSchedule.reduce(
+    (employeePairs, employeeA, index) =>
+      employeePairs.concat(
+        employeesAndSchedule.slice(index + 1).map((employeeB) => {
+          const numberOfMatchesInThePair = employeeA.workedSchedule.reduce(
+            (numberOfCoincidences, dayScheduleEmployeeA) => {
+              const dayScheduleEmployeeB = employeeB.workedSchedule.find(
+                ({ day }) => day === dayScheduleEmployeeA.day
+              )
+              if (!dayScheduleEmployeeB) return numberOfCoincidences
+              if (isMatchInTime(dayScheduleEmployeeA, dayScheduleEmployeeB))
+                return numberOfCoincidences + 1
+              else return numberOfCoincidences
+            },
+            0
+          )
+          return `${employeeA.name}-${employeeB.name}: ${numberOfMatchesInThePair}`
+        })
+      ),
+    []
+  )
+  console.log(employeesMatches)
+  return employeesMatches
 }
 
 const getEmployeesAndScheduleArrayOfStrings = (employeesAndScheduleString) => {
@@ -14,6 +37,40 @@ const getEmployeesAndScheduleArrayOfStrings = (employeesAndScheduleString) => {
     .map((employeeAndScheduleString) => employeeAndScheduleString.trim())
 }
 
+const isMatchInTime = (dayScheduleEmployeeA, dayScheduleEmployeeB) => {
+  const employeeAStartHour = dayScheduleEmployeeA.startHour
+  const employeeAStartMinute = dayScheduleEmployeeA.startMinute
+  const employeeAEndHour = dayScheduleEmployeeA.endHour
+  const employeeAEndMinute = dayScheduleEmployeeA.endMinute
+  const employeeBStartHour = dayScheduleEmployeeB.startHour
+  const employeeBStartMinute = dayScheduleEmployeeB.startMinute
+  const employeeBEndHour = dayScheduleEmployeeB.endHour
+  const employeeBEndMinute = dayScheduleEmployeeB.endMinute
+
+  if (
+    employeeAStartHour > employeeBStartHour &&
+    employeeAStartHour < employeeBEndHour
+  )
+    return true
+  if (
+    employeeAEndHour > employeeBStartHour &&
+    employeeAEndHour < employeeBEndHour
+  )
+    return true
+  if (
+    employeeBStartHour > employeeAStartHour &&
+    employeeBStartHour < employeeAEndHour
+  )
+    return true
+  if (
+    employeeBEndHour > employeeAStartHour &&
+    employeeBEndHour < employeeAEndHour
+  )
+    return true
+  return false
+}
+
 exports.getEmployeesMatchesAtWork = getEmployeesMatchesAtWork
 exports.getEmployeesAndScheduleArrayOfStrings =
   getEmployeesAndScheduleArrayOfStrings
+exports.isMatchInTime = isMatchInTime
--- src/schedule/get-employees-matches-at-work.test.js
+++ src/schedule/get-employees-matches-at-work.test.js
@@ -1,6 +1,7 @@
 const {
   getEmployeesMatchesAtWork,
   getEmployeesAndScheduleArrayOfStrings,
+  isMatchInTime,
 } = require('./get-employees-matches-at-work')
 
 describe('getEmployeesMatchesAtWork', () => {
@@ -36,5 +37,56 @@ describe('getEmployeesAndScheduleArrayOfStrings', () => {
       'ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00',
       'ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00',
     ])
+  })
+})
+
+describe('isMatchInTime', () => {
+  test('Should return true', () => {
+    expect(
+      isMatchInTime(
+        { startHour: 10, startMinute: 0, endHour: 12, endMinute: 0 },
+        { startHour: 11, startMinute: 0, endHour: 13, endMinute: 0 }
+      )
+    ).toBe(true)
+  })
+  test('Should return true', () => {
+    expect(
+      isMatchInTime(
+        { startHour: 10, startMinute: 0, endHour: 12, endMinute: 0 },
+        { startHour: 7, startMinute: 0, endHour: 11, endMinute: 0 }
+      )
+    ).toBe(true)
+  })
+  test('Should return true', () => {
+    expect(
+      isMatchInTime(
+        { startHour: 9, startMinute: 0, endHour: 10, endMinute: 0 },
+        { startHour: 8, startMinute: 0, endHour: 15, endMinute: 0 }
+      )
+    ).toBe(true)
+  })
+  test('Should return true', () => {
+    expect(
+      isMatchInTime(
+        { startHour: 12, startMinute: 0, endHour: 18, endMinute: 0 },
+        { startHour: 14, startMinute: 0, endHour: 15, endMinute: 0 }
+      )
+    ).toBe(true)
+  })
+  test('Should return false', () => {
+    expect(
+      isMatchInTime(
+        { startHour: 10, startMinute: 0, endHour: 14, endMinute: 0 },
+        { startHour: 15, startMinute: 0, endHour: 18, endMinute: 0 }
+      )
+    ).toBe(false)
+  })
+  test('Should return false', () => {
+    expect(
+      isMatchInTime(
+        { startHour: 14, startMinute: 0, endHour: 16, endMinute: 0 },
+        { startHour: 1, startMinute: 0, endHour: 2, endMinute: 0 }
+      )
+    ).toBe(false)
   })
 })
